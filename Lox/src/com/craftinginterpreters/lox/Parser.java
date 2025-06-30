c3d018d65e28154ea08079e4689e7726670bf58c
matteggers <94013259+matteggers@users.noreply.github.com>
2025-06-30 19:15:46 -0400

    partial parser

---
+++ Lox/src/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,35 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+
+    private Expr expression() {
+        return equality();
+    }
+
+    // expression expands to equality rule
+    // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+    // LEFT OFF HERE. ROUGHLY HALF WAY DOWN PDF FOR THIS SECTION
+
+}
